2e1faf982646428ec50016570d14c17d089b6167
buiminhhai1 <ngovietduc20088@gmail.com>
2019-08-29 07:23:06 +0700

    Updates

---
+++ precache-manifest.fbf9a3922dd8581f29fdd6e9cc129adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf29df77d74901cf91022be34f4a71a",
+    "url": "/buiminhhai1/react-component-website.git/index.html"
+  },
+  {
+    "revision": "f02364d301f0ed9e66fc",
+    "url": "/buiminhhai1/react-component-website.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ac441801a35ed81a8e3f",
+    "url": "/buiminhhai1/react-component-website.git/static/js/2.cac30043.chunk.js"
+  },
+  {
+    "revision": "f02364d301f0ed9e66fc",
+    "url": "/buiminhhai1/react-component-website.git/static/js/main.f9657757.chunk.js"
+  },
+  {
+    "revision": "f440b7cef13706ab809f",
+    "url": "/buiminhhai1/react-component-website.git/static/js/runtime~main.ccd213b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/buiminhhai1/react-component-website.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
